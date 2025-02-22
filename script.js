4fd043f1e281608d80002b20d182e789dd4d6093
Ardakaniz <ardakaniz@e.email>
2025-02-22 13:31:50 +0100

    Towards geoloc'

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var map = L.map('map').setView([48.866667, 2.33333], 20);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 15,
@@ -21,12 +19,30 @@ fetch('stations.json')
                     <button>Voir plus</button>
                 </a>
             `);
-            
         });
     })
     .catch(error => {
         console.error('Erreur lors du fetch (mwop) :', error);
     });
 
+let user_location;
+let user_marker;
+function init_gps_watch() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(update_position);
+    }
+    else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
 
+function update_position(pos) {
+    user_location = pos.coords;
 
+    if (user_marker == null) {
+        user_marker = L.circleMarker([user_location.latitude, user_location.longitude], {radius: 10, color: "red"}).addTo(map);
+    }
+    else {
+        user_marker.setLatLng([user_location.latitude, user_location.longitude])
+    }
+}
